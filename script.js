950ddc95ecf1f5eab47c5f046161896f9ab3d334
lavish <lavishjain36@gmail.com>
2022-05-04 16:25:40 +0530

    DOM Form Table insertion

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-let button=document.getElementById("btn");
-button.addEventListener("click",function(){
-  let word=document.getElementById('str').value;
-  //Length of the input 
-  let count=word.length;
-  console.log(count);
+let list1=[];
+let list2=[];
+let list3=[];
+let list4=[];
+let n=1;
+let x=1;
+function AddRow(){
+  let addrown=document.getElementById("show");
+  let newrow=addrown.insertRow(n)
 
-  let outputDiv=document.getElementById('output');
-  outputDiv.innerHTML=`<h1>${count}</h1>`;
-});
+  list1[x]=document.getElementById("fname").value;
+  list2[x]=document.getElementById("lname").value;
+  list3[x]=document.getElementById("email").value;
+  list4[x]=document.getElementById("age").value;
+
+
+  let cell1=newrow.insertCell(0);
+  let cell2=newrow.insertCell(1);
+  let cell3=newrow.insertCell(2);
+  let cell4=newrow.insertCell(3);
+
+  cell1.innerHTML=list1[x];
+  cell2.innerHTML=list2[x];
+  cell3.innerHTML=list3[x];
+  cell4.innerHTML=list4[x];
+
+  document.getElementById("fname").value="";
+  document.getElementById("lname").value="";
+  document.getElementById("email").value="";
+  document.getElementById("age").value="";
+
+}
