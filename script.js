0d547e7e6842763bd62e8b439f6eb4785507d0c7
lavish <lavishjain36@gmail.com>
2022-05-04 14:53:15 +0530

    Added word count using dom

--- script.js
+++ script.js
@@ -1,51 +1,10 @@
-// setTimeout(() => {
-//   console.log('I will execute after 5000 millisecon')
-// }, 5000);
+let button=document.getElementById("btn");
+button.addEventListener("click",function(){
+  let word=document.getElementById('str').value;
+  //Length of the input 
+  let count=word.length;
+  console.log(count);
 
-// 1s=1000ms
-// function displayData(){
-//   console.log("I will execute after 3000 ms")
-// }
-
-
-// let setTimer=setTimeout(displayData,3000)
-
-// i want to cancel SetTimeout 
-
-
-
-
-
-// setTimeout ->web api function that execute your code only for once delay time is over
-
-let i=0;
-let fruits=["papaya","Mangoes","Oranges","Pinapple","Grapes"]
-
-function displayFruit(){
-  console.log(fruits[i])
-  i++;
-}
-
-
-let setTimer =setInterval(displayFruit,3000)
-
-
-setTimeout(() => {
-  clearInterval(setTimer)
-}, 4000);
-
-
-setInterval(() => {
-
-  
-}, interval);
-
-
-setTimeout(() => {
-  
-}, timeout);
-
-setInterval(fun,timeout)
-
-
-Hello .This is a  new change
+  let outputDiv=document.getElementById('output');
+  outputDiv.innerHTML=`<h1>${count}</h1>`;
+});
