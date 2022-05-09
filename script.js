02f9fd752d6f73e6d0686b202823adc86094e807
lavish <lavishjain36@gmail.com>
2022-05-09 15:13:59 +0530

    brewery api

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
-// const url="https://restcountries.com/v3.1/all"
-// fetch(url)
-// .then((response)=>response.json())
-// .then((data)=>{
-//   //Get the dat from the api
-//   console.log(data[92].capital);
-// })
-// .catch((error)=>{
-//   //If there is an error
-//   console.log(error);
-// });
+//Build an html elements
+document.body.innerHTML=`<div class="heading-container">
+<h1>Brewery List</h1>
+<img class="icon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYJk5ZOaUAMfsU2LG0B4x1cC4M3vEsiqQQDQ&usqp=CAU" width="250" height="50" alt="icon">
+</div>
+<div id="mainContainer" class="main-container"></div>
+`;
 
 
-const fetchDataBtn=document.querySelector("#fetchdata");
-const result=document.querySelector("#result");
-
-
-//Get the data from API and set the content of the result div
-async function getData(){
-  result.innerText="Loading....";
+//crate a function to get the data 
+const  getData=async (data) =>{
   try {
-    //Fetch of data code 
-    const res=await fetch("https://api.github.com/users/lavishjain36");
-    const data=await res.json();
-    // result.innerText=`${data.name} has ${data.public_repos} repos`;
-    result.innerText=JSON.stringify(data);
-
+    const data=await fetch("https://api.openbrewerydb.org/breweries");
+    const brewerys=await data.json();
+    brewerys.forEach((brewery)=>{
+      console.log(brewery);
+      displayData(brewery);
+    })
     
   } catch (error) {
-    //Handle error here
-    result.innerText="Error";
+    console.log(error);
     
   }
 
 }
+getData();
 
-//Add a event Listener
-fetchDataBtn.addEventListener("click",function(){
-  getData();
-});
+const displayData=(obj)=>{
+  mainContainer.innerHTML+=
+  `
+  <div class="container">
+  <h3>Brewery Name:<span>${obj.name}</span></h3>
+  <p>Breweris Type:<span>${obj.brewery_type}</span></p>
+  <p>Brewery Address:<span>${obj.street}</span></p>
+  <p>Brewery City:<span>${obj.city}</span></p>
+  <p>Brewery State:<span>${obj.state}</span></p>
+  <p>Brewery Zip:<span>${obj.postal_code}</span></p>
+  <p>Brewery Phone:<span>${obj.phone}</span></p>
+  <p>Brewery Website:<span>${obj.website_url}</span></p>
+  </div>`;
+}
 
